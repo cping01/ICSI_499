c54631c04a6f29c8f06149a93ca2641970c1297e
ajndiaye <142932190+ajndiaye@users.noreply.github.com>
2024-04-08 12:34:09 -0400

    Update AppDataBase.kt

--- AutoInsights/app/src/main/java/com/damc/driver_action/data/local/room/AppDataBase.kt
+++ AutoInsights/app/src/main/java/com/damc/driver_action/data/local/room/AppDataBase.kt
@@ -6,10 +6,43 @@ import com.damc.driver_action.domain.models.ActionData
 import com.damc.driver_action.domain.models.Users
 
 @Database(
-    entities = [Users::class, ActionData::class],
+    entities = [Users::class, ActionData::class, DBTripsegments::class, DBTrip::class, DBTripmetrics::class],
     version = 3,
     exportSchema = false
 )
 abstract class AppDataBase : RoomDatabase() {
     abstract fun OnDataBaseActions(): OnDataBaseActions
+    abstract fun tripDao(): TripDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDataBase? = null
+
+        val MIGRATION_1_2 = object : Migration(1, 2) {
+             override fun migrate(db: SupportSQLiteDatabase) {
+                // Execute  table creation queries 
+                 db.execSQL(
+                         "CREATE TABLE trip_segments( id INTEGER PRIMARY KEY AUTOINCREMENT, tripID INTEGER NOT NULL, distance DOUBLE NOT NULL, duration INTEGER NOT NULL, FOREIGN KEY (tripID) REFERENCES trips(tripID));"
+                         )
+
+                db.execSQL("CREATE TABLE trips (tripID INTEGER PRIMARY KEY AUTOINCREMENT, userID INTEGER, startTime DATETIME NOT NULL, endTime DATETIME,  autoDetected BOOLEAN);" )
+
+                        db.execSQL("CREATE TABLE trip_metrics ( id INTEGER PRIMARY KEY AUTOINCREMENT, tripID INTEGER NOT NULL, averageSpeed DOUBLE, topSpeed DOUBLE, tripLength INTEGER, tripDistance DOUBLE, speedingInstances INTEGER, hardBrakingInstances INTEGER, rapidAccelerationInstances INTEGER,   FOREIGN KEY (tripID) REFERENCES trips(tripID));")
+
+            }
+        }
+
+        fun getDatabase(context: Context): AppDataBase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDataBase::class.java, //  database class here
+                    "AutoInsightsDB"
+                ).build()
+
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
 }
