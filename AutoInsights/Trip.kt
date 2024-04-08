ccc8d1c3e2b9649509447c8fb7e00919e938575d
ajndiaye <142932190+ajndiaye@users.noreply.github.com>
2024-04-08 12:53:31 -0400

    Delete AutoInsights/Trip.kt

--- AutoInsights/Trip.kt
+++
@@ -1,8 +0,0 @@
-@Entity
-data class Trip(
-    @PrimaryKey(autoGenerate = true) val tripID: Int = 0,
-    val userID: Int?, 
-    val startTime: Date,  
-    val endTime: Date?, 
-    val autoDetected: Boolean 
-) 
