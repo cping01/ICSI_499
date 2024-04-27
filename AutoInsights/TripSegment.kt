f358f8d127dbbc7fb7fa855c52d0021401f3a8e3
Abdoulaye Ndiaye <ajndiaye@albany.edu>
2024-04-27 14:11:10 -0400

    the MERGE

--- AutoInsights/TripSegment.kt
+++
@@ -1,7 +0,0 @@
-@Entity
-data class TripSegment(
-    @PrimaryKey(autoGenerate = true) val id: Int = 0,
-    val tripID: Int, 
-    val distance: Double,   
-    val duration: Long  // assuming duration in seconds (hence 'Long')
-) 
